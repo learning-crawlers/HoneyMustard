159c51d6f48fcfe9eb79960cc9388be90623ff1d
Alex B. Santos <alexbenincasa@ymail.com>
2020-02-09 04:51:13 -0300

    test reverse

--- honeymustard/settings.py
+++ honeymustard/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static") 
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [ 'rest_framework.permissions.IsAuthenticatedOrReadOnly' ]
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': [ 'rest_framework.permissions.IsAuthenticatedOrReadOnly' ]
+# }
 
 UNICODE_JSON = True
 
