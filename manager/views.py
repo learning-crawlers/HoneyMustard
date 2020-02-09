88fbb53f94a3a0308dbeece67190718782bf0cb0
Alex B. Santos <alexbenincasa@ymail.com>
2020-02-09 04:54:49 -0300

    test IsAuthenticatedOrReadOnly

--- manager/views.py
+++ manager/views.py
@@ -5,33 +5,29 @@ from django.shortcuts import render
 
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser, IsAuthenticated, BasePermission, SAFE_METHODS
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from manager.models import *
 from manager.serializers import *
-
-class ReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS
 
 class ProxyViewSet(viewsets.ModelViewSet):
     queryset = Proxy.objects.all().order_by('-rank')
     serializer_class = ProxySerializer
-    permission_classes = [ IsAuthenticated or ReadOnly ]
+    permission_classes = [ IsAuthenticatedOrReadOnly ]
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all().order_by('name')
     serializer_class = CategorySerializer
-    permission_classes = [ IsAuthenticated or ReadOnly ]
+    permission_classes = [ IsAuthenticatedOrReadOnly ]
 
 class CrawlerViewSet(viewsets.ModelViewSet):
     queryset = Crawler.objects.all().order_by('name')
     serializer_class = CrawlerSerializer
-    permission_classes = [ IsAuthenticated or ReadOnly ]
+    permission_classes = [ IsAuthenticatedOrReadOnly ]
 
 class LogViewSet(viewsets.ModelViewSet):
     queryset = Log.objects.all().order_by('-created_at')
     serializer_class = LogSerializer
-    permission_classes = [ IsAuthenticated or ReadOnly ]
+    permission_classes = [ IsAuthenticatedOrReadOnly ]
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all().order_by('username')
